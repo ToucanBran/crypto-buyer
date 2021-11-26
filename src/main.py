44202d45b6d8cab2da19f0e93ec4d91abfe84ec9
toucanbran <brandon.gomez89@gmail.com>
2021-11-26 14:24:50 -0800

    changed out async for sync

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@
 # place order if not
 # store order
 
-import sys
+import sys, asyncio
 from load_config import load_config
 from services import setup_logger, get_logger, RabbitMqWrapper, queue, CoinService
 from models.trade_options import TradeOptions
 from services import Buyer, SpotApiWrapper
     
-async def handle_new_coin(ch, method, properties, coin):
+def handle_new_coin(ch, method, properties, coin):
     logger = get_logger("buyer")
     order = order_coin(coin, CoinService())
     logger.info(f"{coin} will be ordered: {order}")
@@ -22,11 +22,11 @@ async def handle_new_coin(ch, method, properties, coin):
     trade_options = TradeOptions()
     trade_options.import_options(configs["trade_options"])
     buyer = Buyer(configs, trade_options, spot_api)
-    price = await buyer.get_last_price(coin, trade_options.pairing)
+    price = asyncio.get_event_loop().run_until_complete(buyer.get_last_price(coin, trade_options.pairing))
     if trade_options.test:
         buyer.place_test_order(coin, price, trade_options)
     else:
-        order = await buyer.place_order(coin, trade_options.pairing, trade_options.quantity,'buy', price)
+        order = asyncio.get_event_loop().run_until_complete(buyer.place_order(coin, trade_options.pairing, trade_options.quantity,'buy', price))
         buyer.store_order(order)
         
     return True
